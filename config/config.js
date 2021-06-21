243c29f9e525394d67bf898268465a4b71aeead4
AndriusDulevicius <andriusdulevicius@email.com>
2021-06-21 11:54:16 +0300

    more changes , on setting up for herocu

--- config/config.js
+++
@@ -1,9 +0,0 @@
-const mongoDbString =
-  'mongodb+srv://andriusAdmin:mongo123@frankfurtclusteraws.9ltyw.mongodb.net/ReactDb?retryWrites=true&w=majority';
-
-// <password> - pakeisti i slaptazodi
-// myFirstDatabase - pakeisti i db pavadinima
-
-module.exports = {
-  mongoDbString,
-};
