91c201397895c738f94bd7902c046bd7e1011b44
AndriusDulevicius <andriusdulevicius@email.com>
2021-06-21 12:02:45 +0300

    static root path added for react front

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const path = require('path');
+
 const cors = require('cors');
 
 const mongoose = require('mongoose');
@@ -16,6 +18,12 @@ app.use(express.json());
 const todoApi = require('./api/todoApi');
 
 app.use('/', todoApi);
+
+const rootBuild = path.join(__dirname, 'client', 'build');
+//pasitikrinti ar musu aplinka yra production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(rootBuild));
+}
 
 // prisijungimas prie duomenu bazes
 mongoose
